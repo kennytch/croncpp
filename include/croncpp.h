55aafc2749c6eb6bcf87381e0d7699a0e247f699
kennytch <kenny.tangchohou@gmail.com>
2022-11-09 12:14:48 +0000

    get rid of dependency of timegm

--- include/croncpp.h
+++ include/croncpp.h
@@ -248,26 +248,53 @@ namespace cron
 
    namespace utils
    {
+      // Algorithm: http://howardhinnant.github.io/date_algorithms.html
+      inline int days_from_epoch(int y, int m, int d)
+      {
+         y -= m <= 2;
+         int era = y / 400;
+         int yoe = y - era * 400;                                   // [0, 399]
+         int doy = (153 * (m + (m > 2 ? -3 : 9)) + 2) / 5 + d - 1;  // [0, 365]
+         int doe = yoe * 365 + yoe / 4 - yoe / 100 + doy;           // [0, 146096]
+         return era * 146097 + doe - 719468;
+      }
+
+      // It  does not modify broken-down time
+      inline std::time_t timegm(std::tm const* t)     
+      {
+         int year = t->tm_year + 1900;
+         int month = t->tm_mon;          // 0-11
+         if (month > 11)
+         {
+            year += month / 12;
+            month %= 12;
+         }
+         else if (month < 0)
+         {
+            int years_diff = (11 - month) / 12;
+            year -= years_diff;
+            month += 12 * years_diff;
+         }
+         int days_since_epoch = utils::days_from_epoch(year, month + 1, t->tm_mday);
+
+         return 60 * (60 * (24L * days_since_epoch + t->tm_hour) + t->tm_min) + t->tm_sec;
+      }
+
       inline std::time_t tm_to_time(std::tm& date)
       {
-#ifdef __USE_MISC
-         return timegm(&date);
-#elif _WIN32
-         return _mkgmtime(&date);
-#else
-         return std::mktime(&date);
-#endif
+         std::time_t rec = utils::timegm(&date);
+         // implicit change to date
+         date = *std::gmtime(&rec);
+         return rec;
       }
 
       inline std::tm* time_to_tm(std::time_t const * date, std::tm* const out)
       {
-#ifdef __USE_MISC
-         return gmtime_r(date, out);
-#elif _WIN32
+#ifdef _WIN32
          errno_t err = gmtime_s(out, date);
          return 0 == err ? out : nullptr;
 #else
-         return localtime_r(date, out);
+         return gmtime_r(date, out);
 #endif
       }
 
